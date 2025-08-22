587be4e202749988baf419c4604a49143d56fb67
GreatDrakes <226545258+GreatDrakes@users.noreply.github.com>
2025-08-23 01:08:27 +0300

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
+"use client";
+
 import "./globals.css";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import Header from "../components/header";
 import { SessionProvider } from "next-auth/react";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Next Ecommerce Store",
-  description: "A simple ecommerce demo built with Next.js, Redux, and NextAuth.",
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
