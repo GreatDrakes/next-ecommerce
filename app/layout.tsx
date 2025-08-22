d981421fc48184224419c2db15de2d94690f446b
GreatDrakes <226545258+GreatDrakes@users.noreply.github.com>
2025-08-22 19:36:31 +0300

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
-import Header from "../components/Header";
+import Header from "../components/header";
 import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/productcard";
 
 async function getProducts() {
   const res = await fetch("https://fakestoreapi.com/products");
